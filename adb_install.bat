ccc07dcb4f2db0ed9623179b9f317de1d0fbf643
dE1l <dell.oxl@gmail.com>
2023-08-19 08:28:10 +0300

    v11 new wifi and gps fix

--- adb_install.bat
+++ adb_install.bat
@@ -9,27 +9,36 @@ cls
 tasklist /nh /fi "imagename eq adb.exe" | find /i "adb.exe" > nul || adb start-server
 echo -------------------------------------------------
 echo Ожидание подключения устройства
-echo 1. Включите режим ADB
+echo 1. Включите режим ADB 
+echo    Для машины ОД в номеронабирателе в ГУ набрать #*32279
+echo    Для прошитого Китая в номеронабирателе в ГУ набрать *1220*
+echo    В открывшемся инженерном меню выбрать четвертый пункт снизу и в нем выключить верхнюю галку и включить нижнюю галку.
+echo    После всех манипуляций нижнюю галку необходимо выключить.
 echo 2. Подключите провод к ГУ
+echo 3. Если устройство не определяется - попробуйте установить драйвер из папки C:\Platform-Tools-Monjaro\Drivers
 echo -------------------------------------------------
 adb wait-for-device
 echo -------------------------------------------------
 adb devices
 echo. 
-rem echo Приложения будут установлены из папки "%defaultLocation%".
 :menu
 echo.
 echo -------------------------------------------------
+echo Всё, что Вы делаете со своим автомобилем, Вы делаете на свой страх и риск. Никто кроме Вас не несет никакой ответственности за все действия.
+echo                  .
 echo Выберите действие:
 echo 1. Установка AutoKit 2022.11.15.1535 (работают кнопки управления музыкой, возможны прерывания звука по беспроводному Аndroid Аuto)
 echo 2. Установка AutoKit 2023.03.20.1121 (не работают кнопки управления музыкой, есть настройка задержки media - исправляет прерывания звука)
-echo 3. Установка Macrodroid + ADB хак write_secure + ADB хак accessibility_services
+echo 3. Установка Macrodroid + ADB хак write_secure + ADB хак accessibility_services + MD Helper
 echo 4. Установка собственных приложений из папки APK (скопируйте необходимые приложения в эту папку)
-echo 5. Carbit хак (отключение на телефоне запроса на трансляцию)
+echo 5. Carbit хак (отключение на телефоне запроса на трансляцию). Делается на телефоне: подключите телефон, а не автомобиль к компьютеру
 echo 6. Отключение проверки доступности интернет соединения (отключение Captive Portal)
 echo 7. Включение проверки доступности интернет соединения (Captive Portal через серверы Яндекс)
-echo 8. ADB хак accessibility_services (запускать после установки и настройки программ, требующих accessibility_services)
+echo 8. ADB хак accessibility_services (для приложений по списку Сергея Bloodyrus)
 echo 9. Установка Галереи и Обоев
+echo 10. Включение модуля WiFi в ГУ
+echo 11. Установка USBGPS4Droid для GPS свистка (нужно только для машин ОД)
+echo 12. Запустить ADB shell (ввод собственных команд в ГУ через ADB)
 echo 0. Выход
 echo -------------------------------------------------
 set "opt=x"
@@ -44,6 +53,9 @@ if %opt%==6 goto CaptivNull
 if %opt%==7 goto CaptivYandex
 if %opt%==8 goto MacrodroidAccess
 if %opt%==9 goto Gallery
+if %opt%==10 goto WIFI
+if %opt%==11 goto GNSS
+if %opt%==12 goto ADBShell
 if %opt%==0 goto processHalt
 echo.
 echo.
@@ -111,9 +123,16 @@ echo.
 echo.
 echo Поздравляю, приложения установлены.
 echo.
-echo Устанавливаю хак write_secure...
+echo Устанавливаю хак...
 	
 	adb shell pm grant com.arlosoft.macrodroid android.permission.WRITE_SECURE_SETTINGS
+	adb shell pm grant com.arlosoft.macrodroid android.permission.CHANGE_CONFIGURATION
+	adb shell pm grant com.arlosoft.macrodroid android.permission.READ_LOGS
+	adb shell pm grant com.arlosoft.macrodroid android.permission.SET_VOLUME_KEY_LONG_PRESS_LISTENER
+	adb shell pm grant com.arlosoft.macrodroid android.permission.DUMP
+	adb shell pm grant com.arlosoft.macrodroid.helper android.permission.WRITE_SECURE_SETTINGS	
+	
+adb shell settings put secure enabled_accessibility_services com.arlosoft.macrodroid/com.arlosoft.macrodroid.triggers.services.MacroDroidAccessibilityServiceJellyBean:com.arlosoft.macrodroid/com.arlosoft.macrodroid.triggers.services.VolumeButtonAccessibilityService:com.arlosoft.macrodroid/com.arlosoft.macrodroid.action.services.UIInteractionAccessibilityService:com.arlosoft.macrodroid/com.arlosoft.macrodroid.triggers.services.FingerprintAccessibilityService
 	
 echo.
 echo Поздравляю, хак установлен.
@@ -202,6 +221,70 @@ adb push WallpaperBloody /storage/emulated/0
 echo.
 echo.
 echo Поздравляю, Галерея и Обои установлены.
+goto menu
+
+:WIFI
+echo.
+echo Включаю WiFi...
+adb root
+adb remount
+adb shell setprop persist.service.wifi.ipcp false
+
+if not EXIST "%defaultLocation%\WiFi\*.apk" ( 
+echo.
+echo Папка не содержит приложений.
+goto changeLocation
+)
+echo Устанавливаю WiFi Manager...
+echo.
+for /f "delims=|" %%i in ('dir /b "%defaultLocation%\WiFi\*.apk"') do (
+    echo -------------------------
+	echo Установка %%~ni
+	adb install -r "%defaultLocation%\WiFi\%%i"
+)
+echo.
+
+echo.
+echo Перезагрузите головное устройство.
+goto menu
+
+:ADBShell
+echo .
+echo Для выхода введите exit
+echo .
+echo .
+
+adb root
+adb shell
+
+echo.
+goto menu
+
+:GNSS
+if not EXIST "%defaultLocation%\GNSS\*.apk" ( 
+echo.
+echo Папка не содержит приложений.
+goto changeLocation
+)
+echo Устанавливаю USBGPS4Droid...
+echo.
+for /f "delims=|" %%i in ('dir /b "%defaultLocation%\GNSS\*.apk"') do (
+    echo -------------------------
+	echo Установка %%~ni
+	adb install -r "%defaultLocation%\GNSS\%%i"
+)
+
+adb shell appops set org.broeuschmeul.android.gps.usb.provider android:mock_location allow
+
+adb shell settings put secure enabled_accessibility_services org.broeuschmeul.android.gps.usb.provider/org.broeuschmeul.android.gps.usb.provider.service.BootService
+
+echo.
+echo.
+echo Поздравляю, GNSS Commander установлен.
+goto menu
+
+echo.
+echo Перезагрузите головное устройство.
 goto menu
 
 :processHalt
